aa3bc77fe1dcd7a9bc35843b439600a415866ef8
OlaVasylieva7 <124078842+OlaVasylieva7@users.noreply.github.com>
2024-04-22 01:35:39 +0200

    add time-calculator and calculator

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import './js/date';
 // import './js/guess';
 // import './js/rsp';
 // import './js/calculator';
-// import './js/time-calculator';
+import './js/time-calculator';
 // import './js/dinosaur';
 // import './js/football';
 // import './js/enter-numbers';
