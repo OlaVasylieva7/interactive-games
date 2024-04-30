07c4e9a072e062aa8e10672bb1fa96bf8aaff4f7
anastasia <167010010+AnastasiaVerbenetss@users.noreply.github.com>
2024-04-30 22:40:20 +0300

    update

---
+++ src/js/calculator.js
@@ -0,0 +1,39 @@
+const calcFirstElem = document.getElementById('first-number');
+const calcSecondElem = document.getElementById('second-number');
+
+function calculateResult() {
+    const calcFirstNumber = +calcFirstElem.value || 0;
+    const calcSecondNumber = +calcSecondElem.value || 0;
+
+    const calcOperatorElem = document.querySelector(
+        'input[name="operator"]:checked'
+    );
+    const calcOperator = calcOperatorElem.value;
+
+    switch (calcOperator) {
+        case '+':
+            return calcFirstNumber + calcSecondNumber;
+        case '-':
+            return calcFirstNumber - calcSecondNumber;
+        case '*':
+            return calcFirstNumber * calcSecondNumber;
+        case '/':
+            return calcSecondNumber !== 0 ? calcFirstNumber / calcSecondNumber : 'ділення на нуль!';
+        default:
+            return '';
+    }
+}
+
+const operatorElements = document.querySelectorAll('input[name="operator"]');
+operatorElements.forEach(elem => {
+    elem.addEventListener('click', function () {
+        buttonResult.disabled = false;
+    });
+});
+
+const buttonResult = document.getElementById('button-result');
+buttonResult.addEventListener('click', function () {
+    const resultValue = document.getElementById('calculator-result');
+    const result = calculateResult();
+    resultValue.innerText = result;
+});
--- src/js/dark-theme.js
+++ src/js/dark-theme.js
@@ -1,14 +1,14 @@
-const icon = document.getElementById("icon");
+// const icon = document.getElementById("icon");
 
-icon.addEventListener("click", onIconClick);
+// icon.addEventListener("click", onIconClick);
 
-function onIconClick() {
-    document.body.classList.toggle("dark-theme");
+// function onIconClick() {
+//     document.body.classList.toggle("dark-theme");
 
-    if (document.body.classList.contains("dark-theme")) {
-        icon.src = "https://raw.githubusercontent.com/OlaVasylieva7/interactive-games/e8b8ec56d1ff175f06f884377424fc9dbc4ac883/src/img/icons/dark-theme.svg";
-        icon.alt = "dark mode";
-    } else {
-        icon.src = "https://raw.githubusercontent.com/OlaVasylieva7/interactive-games/e8b8ec56d1ff175f06f884377424fc9dbc4ac883/src/img/icons/light-theme.svg";
-    }
-}
+//     if (document.body.classList.contains("dark-theme")) {
+//         icon.src = "https://raw.githubusercontent.com/OlaVasylieva7/interactive-games/e8b8ec56d1ff175f06f884377424fc9dbc4ac883/src/img/icons/dark-theme.svg";
+//         icon.alt = "dark mode";
+//     } else {
+//         icon.src = "https://raw.githubusercontent.com/OlaVasylieva7/interactive-games/e8b8ec56d1ff175f06f884377424fc9dbc4ac883/src/img/icons/light-theme.svg";
+//     }
+// }
