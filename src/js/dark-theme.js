d230909b2a66bf98c8fe8091632fb5121119d3f6
anastasia <167010010+AnastasiaVerbenetss@users.noreply.github.com>
2024-04-30 19:42:29 +0300

    update

--- src/js/dark-theme.js
+++ src/js/dark-theme.js
@@ -7,7 +7,7 @@ function onIconClick() {
 
     if (document.body.classList.contains("dark-theme")) {
         icon.src = "https://raw.githubusercontent.com/OlaVasylieva7/interactive-games/e8b8ec56d1ff175f06f884377424fc9dbc4ac883/src/img/icons/dark-theme.svg";
-        icon.alt = "dark mode"
+        icon.alt = "dark mode";
     } else {
         icon.src = "https://raw.githubusercontent.com/OlaVasylieva7/interactive-games/e8b8ec56d1ff175f06f884377424fc9dbc4ac883/src/img/icons/light-theme.svg";
     }
