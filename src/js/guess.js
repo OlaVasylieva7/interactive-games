d4f06bcf3e796393f62f9a177194917a2d127b20
Sabina Belcheva <124079651+SabinaBelcheva@users.noreply.github.com>
2024-05-01 23:00:59 +0300

    enter-number.html, js

--- src/js/guess.js
+++ src/js/guess.js
@@ -1,14 +1,17 @@
-const input = document.getElementById("date__input");
-const dateButton = document.getElementById("date__button");
-const text = document.getElementById("text");
+const input = document.getElementById('date__input');
+const dateButton = document.getElementById('date__button');
+const text = document.getElementById('text');
 
-dateButton.addEventListener("click", onBtnClick);
+dateButton.addEventListener('click', onBtnClick);
 
 function onBtnClick() {
   const randomNumber = Math.floor(Math.random() * 10 + 1);
   let userNumber = Number(input.value);
   if (userNumber === randomNumber) {
     text.textContent = `Вітаю, ви вгадали число! ${randomNumber}`;
-    text.style.color = "#039900"
+    text.style.color = '#039900';
+  } else {
+    text.textContent = `Ви програли, комп'ютер загадав число ${randomNumber}`;
+    text.style.color = '#900';
   }
 }
--- src/main.js
+++ src/main.js
@@ -3,11 +3,11 @@
 // import './js/header';
 import './js/date';
 import './js/guess';
-// import './js/rsp';
+import './js/rsp';
 // import './js/calculator';
 // import './js/time-calculator';
 // import './js/dinosaur';
 // import './js/football';
-// import './js/enter-numbers';
+import './js/enter-numbers';
 // import './js/our-team';
 // import './js/scientists';
