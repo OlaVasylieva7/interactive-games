4bbca144d2d7ab09d5158824ca44d340c4fb31fa
anastasia <167010010+AnastasiaVerbenetss@users.noreply.github.com>
2024-05-02 14:50:28 +0300

    update

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
-// import './js/dark-theme';
+import './js/dark-theme';
 // import './js/modal';
 // import './js/header';
 import './js/date';
 // import './js/guess';
 // import './js/rsp';
-// import './js/calculator';
-// import './js/time-calculator';
+import './js/calculator';
+import './js/time-calculator';
 // import './js/dinosaur';
-// import './js/football';
+import './js/football';
 // import './js/enter-numbers';
-// import './js/our-team';
+import './js/our-team';
 // import './js/scientists';
