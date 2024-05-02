0ca3cc5b3c9d00f6f959842c4a72b693eb4a6f95
anastasia <167010010+AnastasiaVerbenetss@users.noreply.github.com>
2024-05-02 21:43:45 +0300

    footer

---
+++ src/js/modal.js
@@ -0,0 +1,53 @@
+// const modalBtn = document.querySelector('.modal__save-btn');
+const closewModalBtn = document.querySelector('.modal__btn-close');
+// const modal = document.querySelector('.modal');
+// const openModalBtn = document.querySelector('.modal__btn-open');
+// const backdrop = document.querySelector('.js-backdrop');
+// const modalInput = document.querySelector(".modal__input");
+// const output = document.getElementById("user__name");
+const openThanksModalBtn = document.querySelector('[data-action="open-thanks-modal"]');
+const thanksModal = document.querySelector(".thanks__modal")
+// const thanksModal = document.querySelector(".thanks__modal")
+
+
+// openModalBtn.addEventListener('click', onOpenModal);
+// openThanksModalBtn.addEventListener('click', onOpenThanksModal);
+// modalBtn.addEventListener('click', onCloseModal);
+closewModalBtn.addEventListener('click', onCloseModal);
+// backdrop.addEventListener('click', onBackdropClose);
+// modalInput.addEventListener("input", onInputChange);
+
+
+openThanksModalBtn.addEventListener("click", onOpenThanksModal)
+
+
+function onOpenThanksModal() {
+    document.body.classList.add('show-modal');
+}
+// function onOpenModal() {
+//     document.body.classList.add('show-modal');
+//     window.addEventListener('keydown', onEscapePress);
+// }
+
+// // function onOpenThanksModal() {
+// //     document.body.classList.add('show-thanks-modal');
+// //     window.addEventListener('keydown', onEscapePress);
+// // }
+
+function onCloseModal() {
+    document.body.classList.remove('show-modal');
+    window.removeEventListener('keydown', onEscapePress);
+}
+
+// function onBackdropClose(e) {
+//     if (e.target === e.currentTarget) onCloseModal();
+// }
+
+function onEscapePress(e) {
+    e.press;
+    if (e.code === 'Escape') onCloseModal();
+}
+
+// function onInputChange(e) {
+//     output.textContent = e.currentTargent.value || "Незнайомець";
+// }
