df631a1dbf5969e96ab53b12fd9491e493449219
anastasia <167010010+AnastasiaVerbenets@users.noreply.github.com>
2024-05-09 19:05:51 +0300

    update

--- src/js/dark-theme.js
+++ src/js/dark-theme.js
@@ -1,14 +1,19 @@
-const icon = document.getElementById("icon");
+import darkUrl from "../../assets/dark-theme.svg";
+import lightkUrl from "../../assets/light-theme.svg";
 
+
+const icon = document.getElementById("icon");
 icon.addEventListener("click", onIconClick);
 
 function onIconClick() {
     document.body.classList.toggle("dark-theme");
 
     if (document.body.classList.contains("dark-theme")) {
-        icon.src = "/img/icons/dark-theme.svg";
+        icon.src = darkUrl;
+        // icon.src = "/img/icons/dark-theme.svg";
         icon.style.animation
     } else {
-        icon.src = "/img/icons/light-theme.svg";
+        icon.src = lightkUrl;
+        // icon.src = "/img/icons/light-theme.svg";
     }
 }
